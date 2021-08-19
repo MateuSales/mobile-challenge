ea6854e2b99c8e7f7ee800dd89b10c22c9672494
MateuSales <matheussalles40@gmail.com>
2021-08-18 21:06:05 -0300

    Test `viewModel` when text is empty delivers correct values

--- ChallengeMobileTests/ListScene/Presenter/ListPresenterTests.swift
+++ ChallengeMobileTests/ListScene/Presenter/ListPresenterTests.swift
@@ -1,8 +1,23 @@
-//
-//  ListPresenterTests.swift
-//  ChallengeMobileTests
-//
-//  Created by Mateus Sales on 17/08/21.
-//
+import XCTest
+@testable import ChallengeMobile
 
-import Foundation
+class ListPresenterTests: XCTestCase {
+    
+    func test_viewModel_whenTextIsEmpty_deliversCorrecValues() {
+        let viewModel = ListPresenter.viewModel(from: someModel())
+        
+        XCTAssertEqual(viewModel.currencies.count, 2)
+        XCTAssertEqual(viewModel.currencies.first?.initials, "BTC")
+        XCTAssertEqual(viewModel.currencies.first?.description, "Bitcoin")
+        XCTAssertEqual(viewModel.currencies.last?.initials, "EUR")
+        XCTAssertEqual(viewModel.currencies.last?.description, "Euro")
+    }
+    
+    // MARK: - Helpers
+    
+    private func someModel() -> ListModel {
+        let currencyBtc = ListModel.Currency(initials: "BTC", description: "Bitcoin")
+        let currencyEur = ListModel.Currency(initials: "EUR", description: "Euro")
+        return ListModel(currencies: [currencyBtc, currencyEur])
+    }
+}
