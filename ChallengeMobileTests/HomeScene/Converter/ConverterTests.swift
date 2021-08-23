b9ca38fef57c38717cd0e4126da28ee8b12aa6b7
MateuSales <matheussalles40@gmail.com>
2021-08-23 20:42:02 -0300

    Fix test name

--- ChallengeMobileTests/HomeScene/Converter/ConverterTests.swift
+++ ChallengeMobileTests/HomeScene/Converter/ConverterTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class ConverterTests: XCTestCase {
     
-    func test_converter_whenModelIsEmpty_deliversEmptyResult() {
+    func test_calculate_whenModelIsEmpty_deliversEmptyResult() {
         let valueConverted = Converter.calculate(model: nil,
                                                  initials: "BTC",
                                                  valueInDollarString: "10")
